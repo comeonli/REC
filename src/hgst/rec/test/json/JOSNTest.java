28ed6bad76cdd2776704558fc1298111cb520336
JustFY <java.maleli@gmail.com>
2017-10-27 17:16:17 +0800

    2017.10.27

---
+++ src/hgst/rec/test/json/JOSNTest.java
@@ -0,0 +1,23 @@
+/**
+ ********************************************************
+ *                                                      *
+ * Copyright © 2017-2017 HGST.REC   All rights reserved.*
+ *                                                      *
+ * ******************************************************
+ * @Package: hgst.rec.text.json                            
+ * @author: fly                                     
+ * @date: Oct 24, 2017 1:45:41 PM                               
+ 
+ */
+package hgst.rec.test.json;
+
+/**
+ * @author fly
+ *
+ */
+public class JOSNTest {
+	public static void main(String[] args) {
+		
+//		JSONUtil.toJSON("");
+	}
+}
